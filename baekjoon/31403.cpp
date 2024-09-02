1ded1dc931bc03b85b2450165fa641098153a96b
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2024-09-03 02:39:50 +0900

    31403

--- baekjoon/31403.cpp
+++ baekjoon/31403.cpp
@@ -5,6 +5,15 @@ typedef long long ll;
 int main()
 {
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    
+    ll a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b-c<<endl;
+    ll n=0, bb=b;
+    while(bb)
+    {
+        n++;
+        bb/=10;
+    }
+    cout<<(ll)(a*pow(10,n)+b-c)<<endl;
     return 0;
 }
