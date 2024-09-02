ee238b52974679ca24b3b889348c72455b42475d
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2024-09-03 02:43:03 +0900

    27866

---
+++ baekjoon/27866.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    string s;
+    ll n;
+    cin>>s>>n;
+    cout<<s[n-1]<<endl;
+    return 0;
+}
