5c27abe8c3f4a5b3eb9aefd9058482da88346ccb
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2024-09-02 17:07:50 +0900

    test setting for cp and ps

---
+++ baekjoon/b.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n;
+    cin>>n;
+    cout<<n+1<<endl;
+    return 0;
+}
