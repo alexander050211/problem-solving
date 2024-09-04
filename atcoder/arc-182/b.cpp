4c4f8559d6e789843ea3bbc3a8d7a3f4d3289aea
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2024-09-04 15:40:07 +0900

    virtual

---
+++ atcoder/arc-182/b.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define F first
+#define S second
+using namespace std;
+typedef unsigned long long ll;
+typedef pair<ll,ll> pll;
+void solve()
+{
+    ll n,k;
+    cin>>n>>k;
+    ll temp=(1<<k)-1;
+    for(ll i=0; i<n; i++)
+    {
+        if(i>=(1<<(k-1)))
+        {
+            cout<<"1 ";
+            continue;
+        }
+        ll ret=temp;
+        for(int j=0; j<k; j++)
+        {
+            if((i>>j)&1) ret^=(1<<(k-j-2));
+        }
+        cout<<ret<<' ';
+    }
+    cout<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+      solve();
+    }
+    return 0;
+}
